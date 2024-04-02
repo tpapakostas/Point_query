f1ea99660cdae6db3cf91b9a612a0be24b16e72e
tpapakostas <theo.jakee@hotmail.gr>
2024-04-03 00:20:39 +0200

    binary search function

--- Point_query/Point_query.cpp
+++ Point_query/Point_query.cpp
@@ -22,6 +22,31 @@ float euclidean(bim_point pa, bim_point pb) {
     return  sqrt(pow(pa.pos.x-pb.pos.x,2) + pow(pa.pos.y-pb.pos.y,2) + pow(pa.pos.z-pb.pos.z, 2));
 }
 
+
+float find_vertex(const std::vector<edge>& Edges, float target_dist) {
+
+    // Sort the vector of edges based on dist value
+
+    std::vector<edge> sorted_distances = Edges;
+    std::sort(sorted_distances.begin(), sorted_distances.end(), [](const edge& a, const edge& b) {
+        return a.dist < b.dist;
+        });
+
+    // Perform binary search to find the lower bound of the target dist value
+    auto it = std::lower_bound(sorted_distances.begin(), sorted_distances.end(), target_dist,
+        [](const edge& a, float target) { return a.dist < target; });
+
+    // If iterator is valid and points to the target z-value, return its corresponding x-value
+    if (it != sorted_distances.end() && it->dist == target_dist) {
+        return it->p0;
+    }
+    else {
+        // Return some default value to indicate that the target z-value was not found
+        return std::numeric_limits<double>::quiet_NaN(); // Not-a-Number
+    }
+}
+
+
 std::vector<bim_point> bimPoints;
 std::vector<float> InputDistances;
 
@@ -61,10 +86,8 @@ int main() {
         return a.dist < b.dist;
         });
 
-        for (auto& e : edges) {
-            std::cout << e.dist << "\n";
-    }
-
+    
+    
 
 
     
